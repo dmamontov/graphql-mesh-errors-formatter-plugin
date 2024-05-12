c7492b227c14d16b6de4200f96d398e405608015
Dmitrii Mamontov <d@mamontov.tech>
2024-05-12 16:46:53 +0300

    Update version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/namespace,import/no-extraneous-dependencies
 import { constantCase } from 'change-case';
 import { GraphQLError } from 'graphql';
 import { useApolloServerErrors } from '@envelop/apollo-server-errors';
